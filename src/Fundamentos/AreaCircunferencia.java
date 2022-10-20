e47ce7c42743bcb4006cb7097dbaaf276c8c1e63
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2022-10-20 18:45:02 -0300

    Primeiros Exercicios da Cod3r Academy
    Concatenando com as váriaveis.

--- src/Fundamentos/AreaCircunferencia.java
+++ src/Fundamentos/AreaCircunferencia.java
@@ -3,10 +3,14 @@ package Fundamentos;
 public class AreaCircunferencia {
     public static void main(String[] args) {
         double raio = 3.4;
-        double pi = 3.14159;
+        final double pi = 3.14159;
 
         double area = pi * raio * raio;
 
         System.out.println(pi * raio * raio);
+
+        raio = 10;
+        area = pi * raio * raio;
+        System.out.println("Área = " + area + "m2.");
     }
 }
