f9e1aef4816dfa0dcc44b34d732791052fe8f4fe
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2022-10-20 21:49:57 -0300

    Explicação da inferencia.

--- src/Fundamentos/Inferencia.java
+++ src/Fundamentos/Inferencia.java
@@ -7,6 +7,25 @@ public class Inferencia {
         double a = 4.5;
         System.out.println(a);
 
-        var b = 4.5
+        a = 12;
+        System.out.println(a);
+
+        var b = 4.5;
+        System.out.println(b);
+
+        var c = "Texto";
+        System.out.println(c);
+
+        c = "Outro texto";
+        System.out.println(c);
+
+        double d; // variavel declarada
+        d = 123.65; // variavel foi inicializada
+        System.out.println(d); // usada !
+
+        var e;
+        e = 123.45;
+        System.out.println(e);
+
     }
 }
