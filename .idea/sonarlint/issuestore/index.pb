adb7e6011ef61f4fcab5af33d683988d2bfd3bcc
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2022-10-24 18:32:43 -0300

    Atualização 22/10/2022

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+>
+Exercicios.iml,8\0\8064eccbc2d8862acf439471edcbb7f0a7e39b62
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
---
+++ src/Fundamentos/Console.java
@@ -0,0 +1,24 @@
+package Fundamentos;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Console {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bom");
+        System.out.println("dia! \n\n");
+
+        System.out.println("Bom");
+        System.out.println("dia !");
+
+        System.out.println("Megasena: %d %d %d %d %d %d %n", 1, 2, 3, 4, 5, 6);
+        System.out.println("Salário: %.1f%n", 1234.5678);
+        System.out.println("Nome: %s%n", "João");
+
+        int a = 3;
+        Scanner entrada = new Scanner(System.in)
+    }
+}
